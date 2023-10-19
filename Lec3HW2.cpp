d92364c633f9c4d3786c117fe76f95c56c9eb591
Harshit Singh <127868339+Harshit2561@users.noreply.github.com>
2023-10-19 10:50:32 +0530

    Add files via upload

---
+++ Lec3HW2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i=0;
+    int sum=0;
+    while (i<=8){
+        sum=sum+i;
+        i=i+2;
+        
+    }
+cout<<"sum is:"<<sum<<endl;
+}
